b663e2dc5103ac98ccbf0fc75cdd4daff6ae5066
minasmayth <minasmayth@gmail.com>
2022-12-30 13:36:00 +0100

    New updates with daily and monthly data

--- dataloader.py
+++ dataloader.py
@@ -1,16 +1,67 @@
 import pandas as pd
 
-covid_data = pd.read_csv("data/covid_data/COVID-19_aantallen_gemeente_per_dag_tm_03102021.csv", sep=";")
-covid_month_march = covid_data.loc[covid_data["Date_of_publication"].str.startswith("2020-09")]
-covid_month_march = covid_month_march[["Date_of_publication", "Municipality_name",
-                                       "Total_reported", "Deceased"]]
+testing_data = pd.read_csv("data/covid_data/COVID-19_aantallen_gemeente_per_dag_tm_03102021.csv", sep=";")
 
-pop_density_data = pd.read_csv("data/demographic_data/Population_density.csv", sep=",")
+south_holland_testing_data = (testing_data.loc[testing_data["Province"] == "Zuid-Holland"]).drop(
+    labels=["Version", "Date_of_report", "Security_region_code", "Security_region_name",
+            "Municipality_code", "ROAZ_region", "Municipal_health_service", "Province"], axis=1)
 
-covid_sorted = covid_month_march.sort_values(by=["Municipality_name"])
+south_holland_testing_data.dropna(inplace=True)
 
-filtered_data = covid_sorted.loc[covid_sorted['Municipality_name'].isin(pop_density_data["Municipality"].values)]
+SH_daily_data = south_holland_testing_data.loc[
+    south_holland_testing_data["Date_of_publication"].str.startswith("2020-12")]
+SH_daily_data.to_csv("data/cleaned_data/SH_daily_data_dec2020.csv", index=False)
 
-sorted_data = (filtered_data.sort_values(by=["Municipality_name", "Date_of_publication"]))
+SH_testing_data_dec2020 = (SH_daily_data.groupby(["Municipality_name"],as_index=False).sum())
 
-sorted_data.to_csv("data/cleaned_data/covid_september.csv", index=False)
+
+
+age_per_municipality = pd.read_csv("data/demographic_data/Age_per_municipality.csv").drop(
+    labels=["Totaal", "Perioden"], axis=1
+)
+population_density = pd.read_csv("data/demographic_data/Population_density.csv").drop(
+    labels="Year", axis=1
+)
+
+SH_testing_data_dec2020 = SH_testing_data_dec2020.merge(age_per_municipality, how="inner", left_on="Municipality_name"
+                                                        , right_on="Regio's").drop("Regio's", axis=1)
+SH_testing_data_dec2020 = SH_testing_data_dec2020.merge(population_density, how="inner", left_on="Municipality_name"
+                                                        , right_on="Municipality").drop("Municipality", axis=1)
+
+continent_of_origin = pd.read_csv("data/demographic_data/Continent_Of_Origin.csv")
+
+continent_of_origin.dropna(inplace=True)
+
+frames = []
+for municipality in SH_testing_data_dec2020["Municipality_name"].unique():
+    data_slice = (continent_of_origin.loc[continent_of_origin["Region"] == municipality])
+
+    data_slice = data_slice.drop(labels="Region", axis=1).T
+
+    new_header = data_slice.iloc[0] #grab the first row for the header
+    data_slice = data_slice[1:] #take the data less the header row
+    data_slice.columns = new_header #set the header row as the df header
+    data_slice["Region"] = municipality
+
+    frames.append(data_slice)
+
+result = pd.concat(frames)
+SH_testing_data_dec2020 = SH_testing_data_dec2020.merge(result, how="inner", left_on="Municipality_name"
+                                                        , right_on="Region").drop(["Region", "Totaal"], axis=1)
+
+income = pd.read_csv("data/demographic_data/Income.csv").drop("Unnamed: 0", axis=1)
+
+frames =[]
+for municipality in SH_testing_data_dec2020["Municipality_name"].unique():
+    data_slice = (income.loc[income["Regionaam"] == municipality])
+
+    frames.append(data_slice)
+
+result = pd.concat(frames)
+SH_testing_data_dec2020 = SH_testing_data_dec2020.merge(result, how="inner", left_on="Municipality_name"
+                                                        , right_on="Regionaam").drop(["Regionaam"], axis=1)
+
+SH_testing_data_dec2020["Positive tests per 100000"] = \
+    (SH_testing_data_dec2020["Total_reported"] / SH_testing_data_dec2020["Total"])*100000
+
+SH_testing_data_dec2020.to_csv("data/cleaned_data/SH_monthly_data_dec2020.csv", index=False)
